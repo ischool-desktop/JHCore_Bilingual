ea60b9b6672b921bd5614cb22bb9722ea6c6b325
chentaichen <ct.chen@ischool.com.tw>
2015-12-22 10:14:59 +0800

    實驗中學雙語部 JHSchool 客製，copy一份出來修改，匯出/匯入學生基本資料已新寫，將原本註解。

---
+++ JHSchool/StudentExtendControls/Ribbon/StudentImportWizardControls/ImportMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JHSchool.StudentExtendControls.Ribbon.StudentImportWizardControls
+{
+    public enum ImportMode
+    {
+        None,
+        Insert,
+        Update
+    }
+}
